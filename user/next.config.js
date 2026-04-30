bfe127a364b8ac6d2c9f5245735320f00ae6bd97
shahabhusain <shahabhussain098123@gmail.com>
2026-04-30 14:46:09 +0500

    changes

--- user/next.config.js
+++ user/next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
                             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
                             "font-src 'self' https://fonts.gstatic.com https://fonts.googleapis.com",
                             "img-src 'self' blob: data: https://*.clarity.ms https://*.google-analytics.com https://*.googletagmanager.com https://img.youtube.com https://*.ytimg.com",
-                            "connect-src 'self' https://api.800batterypro.com https://*.clarity.ms https://c.bing.com https://*.google-analytics.com",
+                            "connect-src 'self' https://api.800batterypro.com https://*.clarity.ms https://c.bing.com https://*.google-analytics.com https://www.google.com", // ✅ Added www.google.com
                             "frame-src 'self' https://*.googletagmanager.com https://*.youtube.com https://*.youtube-nocookie.com",
                             "child-src 'self' https://*.youtube.com https://*.youtube-nocookie.com",
                         ].join('; ')
