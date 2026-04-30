bfcea0444d7fafbea3759d6867796f23774eeda3
shahabhusain <shahabhussain098123@gmail.com>
2026-04-30 10:10:03 +0500

    changes

--- user/next.config.js
+++ user/next.config.js
@@ -4,7 +4,28 @@ const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    // Add CSP headers to allow Clarity and GTM
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: [
+                            "default-src 'self'",
+                            "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.googletagmanager.com https://*.clarity.ms https://c.bing.com https://www.googletagmanager.com",
+                            "style-src 'self' 'unsafe-inline'",
+                            "img-src 'self' blob: data: https://*.clarity.ms https://*.google-analytics.com https://*.googletagmanager.com",
+                            "connect-src 'self' https://*.clarity.ms https://c.bing.com https://*.google-analytics.com https://*.analytics.google.com",
+                            "font-src 'self'",
+                            "frame-src 'self' https://*.googletagmanager.com",
+                        ].join('; ')
+                    }
+                ]
+            }
+        ];
+    }
 };
  
 module.exports = withNextIntl(nextConfig);
